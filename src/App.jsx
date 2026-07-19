0e56414875c545180aed0a425b0ec2981a69dc0c
Sonal Raj <sonalraj0108@gmail.com>
2026-07-19 11:27:24 +0530

    implemented all the requirements of the porject v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Students from './pages/Students';
+import { StudentProvider } from './context/StudentContext';
+import { Toaster } from 'react-hot-toast';
+import Navbar from './components/Navbar';
+import Dashboard from './pages/Dashboard';
+import StudentList from './pages/StudentList';
+import StudentForm from './pages/StudentForm';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        {/* Navbar could go here */}
-        <Routes>
-          <Route path="/" element={<Students />} />
-          {/* Add other routes as you build them */}
-        </Routes>
-      </div>
-    </Router>
+    <StudentProvider>
+      <Router>
+        <Toaster position="top-right" />
+        <Navbar />
+        <main className="container">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/students" element={<StudentList />} />
+            <Route path="/add" element={<StudentForm />} />
+            <Route path="/edit/:id" element={<StudentForm />} />
+          </Routes>
+        </main>
+      </Router>
+    </StudentProvider>
   );
 }
-
 export default App;
